419871a3ea994b19c68bd4c7276942c7c441773a
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-11-06 20:55:17 -0500

    Update index.js

--- product-service/src/index.js
+++ product-service/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
+const PORT = process.env.PORT || 8081;
 
 dotenv.config();
 
