7e0a7b1eb8c09ab3381eaca97ecc7f01205d021d
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-11-07 12:45:04 -0500

    Update index.js

--- product-service/src/index.js
+++ product-service/src/index.js
@@ -7,9 +7,6 @@ dotenv.config();
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
-app.use(cors());
-app.use(helmet());
-app.use(morgan('combined'));
 
 // Root route for the browser
 app.get('/', (req, res) => {
