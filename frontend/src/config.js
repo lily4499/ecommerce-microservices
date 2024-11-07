4af3a3e262583e8182cd943569c9b25fd65a59ac
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-11-07 09:39:13 -0500

    Create config.js

---
+++ frontend/src/config.js
@@ -0,0 +1,11 @@
+// src/config.js
+const config = {
+  USER_SERVICE_URL: 'http://frontend-lili2024-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/users',
+  PRODUCT_SERVICE_URL: 'http://frontend-lili2024-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/products',
+  ORDER_SERVICE_URL: 'http://frontend-lili2024-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/orders',
+  PAYMENT_SERVICE_URL: 'http://frontend-lili2024-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/payment',
+  INVENTORY_SERVICE_URL: 'http://frontend-lili2024-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/inventory',
+  NOTIFICATION_SERVICE_URL: 'http://frontend-lili2024-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/notification',
+};
+
+export default config;
