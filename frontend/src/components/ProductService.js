5b9963e669cdd370065aeff06ec34304331d8e89
lily4499 <77320224+lily4499@users.noreply.github.com>
2024-11-07 13:39:04 -0500

    Update ProductService.js

--- frontend/src/components/ProductService.js
+++ frontend/src/components/ProductService.js
@@ -1,25 +1,27 @@
 // frontend/src/components/ProductService.js
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProductService = () => {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchProducts = async () => {
             try {
-                const response = await fetch(`${process.env.REACT_APP_PRODUCT_SERVICE_URL}products`);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                const data = await response.json();
-                setProducts(data);
+                const response = await axios.get(`${process.env.REACT_APP_PRODUCT_SERVICE_URL}products`);
+                setProducts(response.data);
             } catch (error) {
                 console.error('Error fetching products:', error);
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchProducts();
     }, []);
+
+    if (loading) return <div>Loading...</div>;
 
     return (
         <div>
